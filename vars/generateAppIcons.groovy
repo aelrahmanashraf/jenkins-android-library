bcf0d64f52726c19c173edd2bcd48fdb2afd69b6
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-08-23 22:47:13 +0300

    Make library more specific for android.

--- vars/generateAppIcons.groovy
+++ vars/generateAppIcons.groovy
@@ -1,15 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(String workspaceDir, String iconPath) {
+def call(String workspaceDir) {
   def iconSizes = ["72x72", "96x96", "128x128", "144x144", "192x192"]
   def iconDirectories = ["mipmap-hdpi", "mipmap-mdpi", "mipmap-xhdpi", "mipmap-xxhdpi", "mipmap-xxxhdpi"]
 
   iconSizes.eachWithIndex { size, index ->
     def outputDirectory = "${workspaceDir}/app/src/main/res/${iconDirectories[index]}"
-
-    def resizedImage = "${outputDirectory}/resized_${size}.png"
-    def outputFileWebp = "${outputDirectory}/ic_launcher.webp"
-
     sh "mkdir -p ${outputDirectory}"
   }
 }
