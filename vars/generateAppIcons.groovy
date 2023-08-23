39e48245dabf4894b058f4f26791d352ea4ebcf6
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-08-24 01:44:04 +0300

    Use for-loop to make code more readable.

--- vars/generateAppIcons.groovy
+++ vars/generateAppIcons.groovy
@@ -4,18 +4,23 @@ def call(String workspaceDir, String iconPath) {
   def iconSizes = ["72x72", "96x96", "128x128", "144x144", "192x192"]
   def iconDirectories = ["mipmap-hdpi", "mipmap-mdpi", "mipmap-xhdpi", "mipmap-xxhdpi", "mipmap-xxxhdpi"]
 
-  iconSizes.eachWithIndex { size, index ->
-    def outputDirectory = "${workspaceDir}/app/src/main/res/${iconDirectories[index]}"
+  for (index in 0..<iconSizes.size()) {
+    def size = iconSizes[index]
+    def directory = iconDirectories[index]
+    def outputDirectory = "${workspaceDir}/app/src/main/res/${directory}"
+
     dir(outputDirectory) {
       sh "mkdir -p ${outputDirectory}"
-      def resizedImage = "${outputDirectory}/${size}.png"
-      def outputFileWebp = "${outputDirectory}/ic_launcher.webp"
 
-      // convert png images to webp format
-      sh "convert ${iconPath} -resize ${size} ${resizedImage}"
-      sh "cwebp -quiet ${resizedImage} -o ${outputFileWebp}"
-      // delete resized images after convertion
-      sh "rm ${resizedImage}"
+      def resizedIcon = "${outputDirectory}/${size}.png"
+      def outputIconWebp = "${outputDirectory}/ic_launcher.webp"
+
+      // resize icon
+      sh "convert ${iconPath} -resize ${size} ${resizedIcon}"
+      sh "cwebp -quiet ${resizedIcon} -o ${outputIconWebp}"
+
+      // delete resized icons after conversion
+      sh "rm ${resizedIcon}"
     }
   }
 }
