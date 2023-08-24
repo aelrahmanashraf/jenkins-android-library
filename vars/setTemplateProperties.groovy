64f7001318564230bce45556c076840f72a8d884
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-08-24 08:19:01 +0300

    Create properties file from template.

--- vars/setTemplateProperties.groovy
+++ vars/setTemplateProperties.groovy
@@ -1,9 +1,28 @@
 #!/usr/bin/env groovy
 
 def call(Map<String, String> propertyMap, String templatePath, String outputPath) {
-  def templateFile = new File(templatePath)
-  if (!templateFile.exists()) {
-    error "Template file not found: ${templateFilePath}"
-    return
+  try {
+    def templateFile = new File(templatePath)
+    if (!templateFile.exists()) {
+      error "Template file not found: ${templatePath}"
+      return
+    }
+
+    def templateContent = templateFile.text
+    propertyMap.each { paramName, envVarName ->
+      def envVarValue = env."${envVarName}"
+
+      if (envVarValue != null) {
+        templateContent = templateContent.replaceAll(paramName, envVarValue)
+      } else {
+        error "Environment variable ${envVarName} not found."
+      }
+
+      def outputFile = new File(outputPath)
+      outputFile.text = templateContent
+    }
+  } catch (Exception e) {
+    currentBuild.result = 'FAILURE'
+    error "Error in populating template: ${e.message}"
   }
 }
