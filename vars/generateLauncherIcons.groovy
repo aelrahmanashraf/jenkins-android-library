4f833690d69380ca45323b10f6a12496e285a30d
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-08-24 05:39:19 +0300

    Generate round version of launcher icon.

--- vars/generateLauncherIcons.groovy
+++ vars/generateLauncherIcons.groovy
@@ -2,14 +2,14 @@
 
 def call(String resDirectory, String iconPath) {
   try {
-    generateIcons(resDirectory, iconPath)
+    generateLauncherIcons(resDirectory, iconPath)
   } catch (Exception e) {
     echo "An error occurred while generating icons: ${e.message}"
     currentBuild.result = 'FAILURE'
   }
 }
 
-def generateIcons(String resDirectory, String iconPath) {
+def generateLauncherIcons(String resDirectory, String iconPath) {
   def iconSizes = ["72x72", "96x96", "128x128", "144x144", "192x192"]
   def iconDirectories = ["mipmap-hdpi", "mipmap-mdpi", "mipmap-xhdpi", "mipmap-xxhdpi", "mipmap-xxxhdpi"]
 
@@ -17,8 +17,19 @@ def generateIcons(String resDirectory, String iconPath) {
     def outputIconDirectory = "${resDirectory}/${iconDirectories[index]}"
 
     dir(outputIconDirectory) {
-      def outputIconPath = "${outputIconDirectory}/ic_launcher.webp"
-      sh "convert ${iconPath} -resize ${size} -filter Lanczos -quality 90 ${outputIconPath}"
+      def outputIcon = "ic_launcher.webp"
+      def outputRoundIcon = "ic_launcher_round.webp"
+
+      generateWebP(iconPath, size, outputIcon)
+      generateRoundWebP(iconPath, size, outputRoundIcon)
     }
   }
+}
+
+def generateWebP(String inputPath, String size, String outputPath) {
+  sh "convert ${inputPath} -resize ${size} -filter Lanczos -quality 100 ${outputPath}"
+}
+
+def generateRoundWebP(String inputPath, String size, String outputPath) {
+  sh "convert ${inputPath} -resize ${size} -alpha set -background none -vignette 0x0 -filter Lanczos -quality 100 ${outputPath}"
 }
