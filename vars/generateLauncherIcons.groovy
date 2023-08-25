c41f2963255df601cd64faacd52a068eb9fa0fa4
Abdelrahman Ashraf <abdelrahman@picassos.xyz>
2023-08-25 12:07:42 +0300

    Separate call into functions and handle errors.

--- vars/generateLauncherIcons.groovy
+++ vars/generateLauncherIcons.groovy
@@ -1,29 +1,40 @@
 #!/usr/bin/env groovy
 
+def ICON_SIZES = ["72x72", "96x96", "128x128", "144x144", "192x192"]
+def ICON_DIRECTORIES = ["mipmap-hdpi", "mipmap-mdpi", "mipmap-xhdpi", "mipmap-xxhdpi", "mipmap-xxxhdpi"]
+
 def call(String resDirectory, String iconPath) {
   try {
+    validateInput(resDirectory, iconPath)
     generateLauncherIcons(resDirectory, iconPath)
+  } catch (FileNotFoundException e) {
+    echo "Icon file not found: ${iconPath}"
+    handleFailure()
   } catch (Exception e) {
-    echo "An error occurred while generating icons: ${e.message}"
-    currentBuild.result = 'FAILURE'
+    echo "An error occurred: ${e.message}"
+    handleFailure()
   } finally {
     cleanTempDirectories(resDirectory)
   }
 }
 
-def generateLauncherIcons(String resDirectory, String iconPath) {
-  def iconSizes = ["72x72", "96x96", "128x128", "144x144", "192x192"]
-  def iconDirectories = ["mipmap-hdpi", "mipmap-mdpi", "mipmap-xhdpi", "mipmap-xxhdpi", "mipmap-xxxhdpi"]
+def validateInput(String resDirectory, String iconPath) {
+  if (!new File(iconPath).exists()) {
+    throw new FileNotFoundException("Icon file not found: ${iconPath}")
+  }
 
-  iconSizes.eachWithIndex { size, index ->
-    def outputIconDirectory = "${resDirectory}/${iconDirectories[index]}"
+  if (!new File(resDirectory).isDirectory()) {
+    throw new IllegalArgumentException("Invalid resource directory: ${resDirectory}")
+  }
+}
+
+def generateLauncherIcons(String resDirectory, String iconPath) {
+  ICON_SIZES.eachWithIndex { size, index ->
+    def outputIconDirectory = "${resDirectory}/${ICON_DIRECTORIES[index]}"
 
     dir(outputIconDirectory) {
-      def outputIcon = "ic_launcher.webp"
-      def outputRoundIcon = "ic_launcher_round.webp"
-
-      generateWebP(iconPath, size, outputIcon)
-      generateRoundWebP(iconPath, size, outputRoundIcon)
+      generateWebP(iconPath, size, "ic_launcher.webp")
+      generateRoundWebP(iconPath, size, "ic_launcher_round.webp")
     }
   }
 }
@@ -37,7 +48,11 @@ def generateRoundWebP(String inputPath, String size, String outputPath) {
 }
 
 def cleanTempDirectories(String resDirectory) {
-  dir("${resDirectory}") {
+  dir(resDirectory) {
     sh "rm -rf mipmap-*@tmp"
   }
+}
+
+def handleFailure() {
+  currentBuild.result = 'FAILURE'
 }
